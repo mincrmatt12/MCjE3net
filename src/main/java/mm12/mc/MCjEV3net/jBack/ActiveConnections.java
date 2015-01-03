8ab59ea19185c49a8e6fb4dc0afe0be28ab60efd
mincrmatt12 <matthewmirvish@hotmail.com>
2015-01-03 21:19:46 -0100

    gfgfssssjk

--- src/main/java/mm12/mc/MCjEV3net/jBack/ActiveConnections.java
+++ src/main/java/mm12/mc/MCjEV3net/jBack/ActiveConnections.java
@@ -1,5 +1,32 @@
 package mm12.mc.MCjEV3net.jBack;
 
-public class ActiveConnections {
+import java.util.HashMap;
+import java.util.Map;
 
+import mm12.jEV3net.Connection;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ActiveConnections {
+	 public Map<Integer, Connection> connections = new HashMap<Integer, Connection>();
+	 
+	 public Connection getConn(ItemStack card){
+		 if (card.getTagCompound() != null){
+			 
+			 NBTTagCompound data = card.getTagCompound();
+			 
+			 int toId = data.getInteger("id");
+			 
+			 if (connections.containsKey(toId)){
+				 return connections.get(toId);
+			 }
+			 else{
+				 // add connection code here....
+				 
+			 }
+			 
+		 }
+		 return null;
+	 }
+	
 }
